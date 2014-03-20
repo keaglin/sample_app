9f67753d47ee972cf06acefe8709d6c6c593b321
K. Eaglin <kceaglin@gmail.com>
2014-03-20 18:38:17 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,7 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
 
   it { should be_valid }
 
@@ -106,6 +107,11 @@ describe "when email address is already taken" do
       @user.save
       expect(@user.reload.email).to eq mixed_case_email.downcase
     end
+  end
+  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
   
   
