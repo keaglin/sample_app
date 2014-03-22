f0b79baf87deea8195076e957377787d81dab0dd
K. Eaglin <kceaglin@gmail.com>
2014-03-22 10:36:44 -0400

    Chapter 8 Exercises

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  #force_ssl
+
   include SessionsHelper
+
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,11 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by(email: params[:session][:email].downcase)
-		if user && user.authenticate(params[:session][:password])
+		user = User.find_by_email(params[:email].downcase)
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
 			sign_in user
-			redirect_to user
+			redirect_to user, :notice => 'Logged in!'
 		else
 			flash.now[:error] = 'Invalid email/password combination'
 		render 'new'
@@ -16,7 +17,8 @@ class SessionsController < ApplicationController
 
 	def destroy
 		sign_out
-		redirect_to root_url
+		session[:user_id] = nil
+		redirect_to root_url, :notice => 'Logged out!'
 	end
 	
 end
